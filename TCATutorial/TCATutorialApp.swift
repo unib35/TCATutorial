adc7a79ec3d282d4a2b07cef4df33f448119e6c1
Jong MIn <unib335@naver.com>
2025-02-14 15:52:26 +0900

    init commit

---
+++ TCATutorial/TCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialApp.swift
+//  TCATutorial
+//
+//  Created by 이종민 on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
